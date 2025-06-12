68da59d98b9d2f1a4d841063cedb16c4f6c6517f
nicoprogramming1 <wnorowsky.nicolas@gmail.com>
2025-06-12 12:02:17 -0300

    Init Spring Boot project

---
+++ src/main/java/com/technical_test/encodelabs/EncodelabsApplication.java
@@ -0,0 +1,15 @@
+package com.technical_test.encodelabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncodelabsApplication {
+   
+   public static void main(String[] args) {
+      SpringApplication.run(EncodelabsApplication.class, args);
+      System.out.println("Running");
+   }
+   
+}
+/**/
---
+++ src/test/java/com/technical_test/encodelabs/EncodelabsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technical_test.encodelabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncodelabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
