0de984ec85bbea54d3a2aaf5a4b18e2648425090
nicoprogramming1 <wnorowsky.nicolas@gmail.com>
2025-06-12 21:46:57 -0300

    implement a standard ApiResponse dto

---
+++ src/main/java/com/technical_test/encodelabs/dto/ApiResponse.java
@@ -0,0 +1,26 @@
+package com.technical_test.encodelabs.dto;
+
+import java.util.List;
+
+/**
+ * Es un record para mantener la inmutabilidad, además genera automáticamente el
+ * boiler plate, siendo muy liviana pero con la seguridad de una clase
+ * @param success se asigna automáticamente dependiendo si se llama success / failure
+ * @param message de la operación
+ * @param data con la información a presentar como un array, en caso de no devolver info se asigna []
+ * @param <T> genérico, será el tipo de la data (List<ProductResponseDTO> por ej)
+ */
+public record ApiResponse<T>(
+        boolean success,
+        String message,
+        T data
+) {
+   
+   public static <T> ApiResponse<List<T>> success(String message, List<T> data) {
+      return new ApiResponse<>(true, message, data != null ? data : List.of());
+   }
+   
+   public static <T> ApiResponse<List<T>> failure(String message, List<T> data) {
+      return new ApiResponse<>(false, message, data != null ? data : List.of());
+   }
+}
