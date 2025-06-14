7d70cb441afaffae75fe097c565fb7f97fc6035a
nicoprogramming1 <wnorowsky.nicolas@gmail.com>
2025-06-14 20:06:29 -0300

    add a product domain repository (need to fix domain-only methods)

---
+++ src/main/java/com/technical_test/encodelabs/repository/ProductRepository.java
@@ -0,0 +1,35 @@
+package com.technical_test.encodelabs.repository;
+
+import com.technical_test.encodelabs.model.Product;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+/**
+ * Esta interfaz define un repositorio de dominio y declara el
+ * comportamiento que debe tener un producto, np se rela ciona
+ * de forma directa con JPA, reduce acoplamiento
+ */
+public interface ProductRepository {
+   
+   Product save(Product product);   // crea o actualiza
+   
+   List<Product> findAll();
+   
+   List<Product> findActiveAll();
+   
+   List<Product> findInactiveAll();
+   
+   Optional<Product> findById(UUID id);
+   
+   UUID deleteById(UUID id);
+   
+   UUID deactivateById(UUID id);
+   
+   UUID reactivateById(UUID id);
+   
+   boolean existsById(UUID id);
+   
+   boolean isActive(UUID id);
+}
