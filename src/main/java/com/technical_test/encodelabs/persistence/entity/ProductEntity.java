5588ae92da56952d960675a54247ec1b721db073
nicoprogramming1 <wnorowsky.nicolas@gmail.com>
2025-06-14 12:40:20 -0300

    add Product JPA Entity

---
+++ src/main/java/com/technical_test/encodelabs/persistence/entity/ProductEntity.java
@@ -0,0 +1,54 @@
+package com.technical_test.encodelabs.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "products")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductEntity {
+   
+   @Id
+   private UUID id;
+   
+   @Column(nullable = false, length = 100)
+   private String name;
+   
+   @Column(length = 300)
+   private String description;
+   
+   @Column(nullable = false, precision = 10, scale = 2)
+   private BigDecimal priceAmount;
+   
+   @Column(nullable = false)
+   private String currencyCode; // decidí persistirlo
+   
+   @Column(nullable = false)
+   private Integer quantity;
+   
+   @Column(nullable = false, updatable = false)
+   private LocalDateTime createdAt;
+   
+   @Column(nullable = false)
+   private LocalDateTime updatedAt;
+   
+   // auto generemos timestamps en operaciones save or update
+   @PrePersist
+   public void onCreate() {
+      this.createdAt = this.updatedAt = LocalDateTime.now();
+   }
+   
+   @PreUpdate
+   public void onUpdate() {
+      this.updatedAt = LocalDateTime.now();
+   }
+   
+}
