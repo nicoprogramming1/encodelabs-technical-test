348a96648e66d5460185bbf0ab1091a69f492d00
nicoprogramming1 <wnorowsky.nicolas@gmail.com>
2025-06-15 12:06:26 -0300

    add ProductRegisterRequestDTO with swagger and jakarta validation

---
+++ src/main/java/com/technical_test/encodelabs/dto/Product/ProductRegisterRequestDTO.java
@@ -0,0 +1,33 @@
+package com.technical_test.encodelabs.dto.Product;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.*;
+
+import java.math.BigDecimal;
+
+/**
+ * el record crea boilerplate automáticamente, podría ponerle msg a cada validación o x defecto
+ * Validamos mediante jakarta validation cada input
+ */
+public record ProductRegisterRequestDTO(
+        @NotNull
+        @Size(min = 3, max = 100)
+        @Schema(description = "Product name", example = "Shirt")
+        String name,
+        
+        @Size(max = 300)
+        @Schema(description = "Product optional description", example = "White comfortable shirt")
+        String description,
+        
+        @NotNull
+        @DecimalMin(value = "0.01", inclusive = true) // por defecto true pero lo dejo por legibilidad
+        @Digits(integer = 10, fraction = 2)
+        @Schema(description = "Product price amount", example = "50.00")
+        BigDecimal priceAmount,
+        
+        @NotNull
+        @Min(0)
+        @Schema(description = "Product quantity", example = "10")
+        Integer quantity
+) {
+}
