09eacbd846973d5eab539c628c8bec64810e9cc3
nicoprogramming1 <wnorowsky.nicolas@gmail.com>
2025-06-15 17:23:48 -0300

    replace logs and implement pagination

--- src/main/java/com/technical_test/encodelabs/service/ProductsService.java
+++ src/main/java/com/technical_test/encodelabs/service/ProductsService.java
@@ -1,36 +1,48 @@
 package com.technical_test.encodelabs.service;
 
+import com.technical_test.encodelabs.common.util.LogInfo;
+import com.technical_test.encodelabs.dto.PaginatedResponseDTO;
+import com.technical_test.encodelabs.dto.Product.ProductRegisterRequestDTO;
 import com.technical_test.encodelabs.dto.Product.ProductResponseDTO;
 import com.technical_test.encodelabs.model.Product;
 import com.technical_test.encodelabs.persistence.mapper.ProductResponseMapper;
 import com.technical_test.encodelabs.repository.ProductRepository;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
-@Slf4j
 @Service
 public class ProductsService {
    private final ProductRepository productRepository;
    private final ProductResponseMapper mapper;
-   private final MessageService msgService;
+   private final LogInfo log;
+   private final String className = this.getClass().getName();
+   
    
    public ProductsService(
            ProductRepository productRepository,
            ProductResponseMapper mapper,
-           MessageService msgService
+           LogInfo log
    ) {
       this.productRepository = productRepository;
       this.mapper = mapper;
-      this.msgService = msgService;
+      this.log = log;
    }
    
-   public List<ProductResponseDTO> retrieveAll() {
-      List<Product> products = productRepository.findAll();
-      List<ProductResponseDTO> productResponseDTOS = mapper.toResponseList(products);
+   
+   public ProductResponseDTO create(ProductRegisterRequestDTO requestDTO) {
+      Product newProduct = Product.create(requestDTO);
+      Product savedPoduct = productRepository.save(newProduct);
       
-      log.info("{}: {}", msgService.get("product.list"), products);
-      return productResponseDTOS;
+      log.logInfoAction("product.saved", savedPoduct, className);
+      return mapper.toResponse(savedPoduct);
+   }
+   
+   public PaginatedResponseDTO<ProductResponseDTO> retrieveAll(Pageable pageable) {
+      Page<Product> page = productRepository.findAll(pageable);
+      PaginatedResponseDTO<ProductResponseDTO> response = mapper.toPaginatedResponse(page);
+      
+      log.logInfoAction("product.list", page, className);
+      return response;
    }
 }
