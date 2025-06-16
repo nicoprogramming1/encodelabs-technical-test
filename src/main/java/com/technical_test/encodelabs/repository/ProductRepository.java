a6cfc3cde0e6355169b2ad05f827ed31d51d4f72
nicoprogramming1 <wnorowsky.nicolas@gmail.com>
2025-06-16 13:10:05 -0300

    fix return types from ProductEntity to Product (only repo should interact with ProductEntity)

--- src/main/java/com/technical_test/encodelabs/repository/ProductRepository.java
+++ src/main/java/com/technical_test/encodelabs/repository/ProductRepository.java
@@ -1,7 +1,6 @@
 package com.technical_test.encodelabs.repository;
 
 import com.technical_test.encodelabs.model.Product;
-import com.technical_test.encodelabs.persistence.entity.ProductEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -9,34 +8,17 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-/**
- * Esta interfaz define un repositorio de dominio y declara el
- * comportamiento que debe tener un producto, np se rela ciona
- * de forma directa con JPA, reduce acoplamiento
- */
 public interface ProductRepository {
-   
-   ProductEntity save(Product product);   // crea o actualiza
-   
-   void saveAll(List<Product> products); // para seeder (podría usarse para crear múltiples products)
-   
-   Page<ProductEntity> findAll(Pageable pageable);
-   
-   List<ProductEntity> findActiveAll();
-   
-   List<ProductEntity> findInactiveAll();
-   
-   Optional<ProductEntity> findById(UUID id);
-   
+   Product save(Product product);
+   void saveAll(List<Product> products);  // para seeder pero podría usarse para inserts múltiples
+   Page<Product> findAll(Pageable pageable);
+   List<Product> findActiveAll();
+   List<Product> findInactiveAll();
+   Optional<Product> findById(UUID id);
    UUID deleteById(UUID id);
-   
    UUID deactivateById(UUID id);
-   
    UUID reactivateById(UUID id);
-   
    boolean existsById(UUID id);
-   
    boolean isActive(UUID id);
-   
    Long count();
 }
