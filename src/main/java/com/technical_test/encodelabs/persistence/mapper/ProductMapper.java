822aa14d97eb1bbae8430640c707549b82dc1b80
nicoprogramming1 <wnorowsky.nicolas@gmail.com>
2025-06-16 13:10:44 -0300

    add updateFromDTO to map between an update dto and a Product to be saved

--- src/main/java/com/technical_test/encodelabs/persistence/mapper/ProductMapper.java
+++ src/main/java/com/technical_test/encodelabs/persistence/mapper/ProductMapper.java
@@ -1,11 +1,14 @@
 package com.technical_test.encodelabs.persistence.mapper;
 
+import com.technical_test.encodelabs.dto.Product.ProductRegisterRequestDTO;
 import com.technical_test.encodelabs.model.Money;
 import com.technical_test.encodelabs.model.Product;
 import com.technical_test.encodelabs.model.builder.ProductBuilder;
 import com.technical_test.encodelabs.persistence.entity.ProductEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+
+import java.math.BigDecimal;
 
 /**
  * Se encarga de mapear a ProductEntity para guardar el producto en db
@@ -21,6 +24,7 @@ public abstract class ProductMapper {
    
    @Mapping(target = "priceAmount", source = "price", qualifiedByName = "mapToAmount")
    @Mapping(target = "currencyCode", source = "price", qualifiedByName = "mapToCurrencyCode")
+   @Mapping(target = "isActive", source = "active")
    public abstract ProductEntity toEntity(Product product);
    
    public Product toDomain(ProductEntity entity) {
@@ -36,4 +40,27 @@ public abstract class ProductMapper {
       );
    }
    
+   /**
+    * Mapper para update de un producto, tomando desde el dto que llega
+    * los datos a actualizar. El campo updatedAt se actualiza de manera automática (@PostPersist)
+    * El isActive sólo puede actualizarse por su propio endpoint
+    * @param productFromDto que viene de la request
+    * @param retrievedProduct como resultado del mapping
+    * @return un Product con info actualizada
+    */
+   public Product updateFromDTO(ProductRegisterRequestDTO productFromDto, Product retrievedProduct) {
+      BigDecimal priceAmount = new BigDecimal(String.valueOf(productFromDto.priceAmount()));
+      Money price = new Money(priceAmount);
+      System.out.println("SAVE received product: " + retrievedProduct);
+      
+      return ProductBuilder.forRetrievedProduct(
+              retrievedProduct.getId(),
+              productFromDto.name(),
+              productFromDto.description(),
+              price,
+              productFromDto.quantity(),
+              retrievedProduct.isActive(),
+              retrievedProduct.getUpdatedAt()
+      );
+   }
 }
