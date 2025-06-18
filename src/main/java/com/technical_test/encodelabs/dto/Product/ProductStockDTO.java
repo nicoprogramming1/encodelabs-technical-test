7664a793c78542c7dd6bb4eb5a524efed80499ca
nicoprogramming1 <wnorowsky.nicolas@gmail.com>
2025-06-18 10:29:00 -0300

    add swagger schema

--- src/main/java/com/technical_test/encodelabs/dto/Product/ProductStockDTO.java
+++ src/main/java/com/technical_test/encodelabs/dto/Product/ProductStockDTO.java
@@ -1,5 +1,6 @@
 package com.technical_test.encodelabs.dto.Product;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
@@ -8,6 +9,7 @@ public record ProductStockDTO(
         @Min(value = -1000)
         @Max(value = 1000)
         @NotNull
+        @Schema(description = "Product stock", example = "50")
         Integer stock
 ) {
 }
