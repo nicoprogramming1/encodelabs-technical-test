0073d8668d5edd57a10b86d55c5ef912df413d88
nicoprogramming1 <wnorowsky.nicolas@gmail.com>
2025-06-18 12:12:43 -0300

    swagger isn't working........

--- run.sh
+++ run.sh
@@ -13,23 +13,27 @@ until [ "$(docker inspect -f '{{.State.Health.Status}}' encodelabs-api)" == "hea
 done
 
 echo "API saludable, corriendo tests Newman..."
-docker compose up --abort-on-container-exit postman-tests
+docker compose up -d postman-tests
 
+echo "Logs de tests:"
+docker compose logs -f postman-tests
 
-exit_code=$?
+exit_code=$(docker inspect postman-tests --format='{{.State.ExitCode}}')
 
-# Resultado
 if [ $exit_code -ne 0 ]; then
   echo "Tests fallaron"
 else
   echo "Tests pasaron correctamente"
 fi
 
+echo ""
 echo "   APP Running!!"
 echo ""
-echo "   • API URL: http://localhost:8080/api"
-echo "   • Swagger UI: http://localhost:8080/api/swagger-ui.html"
+echo "   • API URL products: http://localhost:8080/api/products"
+echo "   • no funciona :( -> Swagger UI: http://localhost:8080/api/swagger-ui.html"
 echo "   • Health Check: http://localhost:8080/api/actuator/health"
 echo ""
-echo "Para ver los logs: docker-compose logs -f"
-echo "Para bajar el container: docker compose down"
+echo "  Para ver los logs: docker-compose logs -f"
+echo "  Para bajar el container: docker compose down"
+
+tail -f /dev/null
